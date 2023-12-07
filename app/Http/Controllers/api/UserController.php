15de593b29614b35ba5c6fde3f91022379a087fe
Binta <fbinta108@gmail.com>
2023-12-07 13:56:53 +0000

    Athentification

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -1,10 +1,13 @@
 <?php
 
 namespace App\Http\Controllers\api;
+
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Faker\Core\File;
+use GuzzleHttp\Promise\Create;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -19,9 +22,51 @@ class UserController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function login(Request $request)
     {
-        //
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $user =  User::where('email', $request->email)->first();
+
+        if ($user) {
+            if (Hash::check($request->password, $user->password)) {
+                if ($user->role === 'Bailleur') {
+
+                    $token = $user->createToken('auth_token')->plainTextToken;
+                    return response()->json([
+                        "statut" => 1,
+                        "massage" => "Vous êtes connecté en tant que Bailleur",
+                        "token" => $token
+                    ]);
+                } elseif ($user->role === 'Porteur') {
+                    $token = $user->createToken('auth_token')->plainTextToken;
+                    return response()->json([
+                        "statut" => 1,
+                        "massage" => "Vous êtes connecté en tant que Porteur de projet",
+                        "token" => $token
+                    ]);
+                } else {
+                    $token = $user->createToken('auth_token')->plainTextToken;
+                    return response()->json([
+                        "statut" => 1,
+                        "massage" => "Vous êtes connecté en tant que Admin",
+                        "token" => $token
+                    ]);
+                }
+            } else {
+                return response()->json([
+                    "statut" => 0,
+                    "massage" => "Mot de passe Incorrect"
+                ]);
+            }
+        } else {
+            return response()->json([
+                "massage" => "Email Introuvable"
+            ]);
+        }
     }
 
     /**
@@ -41,27 +86,26 @@ class UserController extends Controller
             'organisme' => 'nullable|in:ONG,Entreprise,Particulier',
         ]);
 
-        $user= new User();
-        $user->name= $request->name;
-        $user->email= $request->email;
-        $user->password= $request->password;
-        $user->description= $request->description;
-        $user->telephone= $request->telephone;
-        $user->role= $request->role;
-        $user->organisme= $request->organisme;
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->description = $request->description;
+        $user->telephone = $request->telephone;
+        $user->role = $request->role;
+        $user->organisme = $request->organisme;
         $imageData = $request->image;
         $imageName = time() . '.jpeg';
         file_put_contents(public_path('image/' . $imageName), $imageData);
         $user->image = "image/" . $imageName;
 
-        if($user->save()){
+        if ($user->save()) {
             return response()->json([
-                "status"=>"ok",
-                "message"=>"c'est bon",
+                "status" => "ok",
+                "message" => "c'est bon",
                 "data" => $user
-           ]);
+            ]);
         }
-      
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+
 Route::post('/inscription', [UserController::class, 'store']);
+
+Route::post('/login', [UserController::class, 'login'] );
