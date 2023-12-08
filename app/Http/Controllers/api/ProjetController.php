ca31c9d0b7c4f42a03ff213caf667804b03a49ff
Tidiane Badji <btidianed004@gmail.com>
2023-12-08 18:11:52 +0000

    Ajouter un projet | Moifier un projet

--- app/Http/Controllers/api/ProjetController.php
+++ app/Http/Controllers/api/ProjetController.php
@@ -5,6 +5,7 @@ use App\Models\User;
 use App\Models\Projet;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use SebastianBergmann\CodeCoverage\Report\Xml\Project;
 
 class ProjetController extends Controller
 {
@@ -25,35 +26,27 @@ class ProjetController extends Controller
      */
     public function store(Request $request): \Illuminate\Http\JsonResponse
     {
-        $request->validate([
-            'nom' => 'required',
-            'objectif' => 'required',
-            'description' => 'required',
-            'echeance' => 'required',
-            'budget' => 'required|numeric',
-            'etat' => 'in:Disponible,Financé',
-            'categories_id' => 'required|exists:categories,id',
-        ]);
-
         //On crée un nouvel projet
-        $projet = Projet::create([
-            'nom' => $request->name,
-            'objectif' => $request->objectif,
-            'description' => $request->description,
-            'echeance' => $request->echeance,
-            'budget' => $request->budget,
-            'etat' => $request->etat,
-            'categorie_id'
-        ]);
+        $projet = new Projet();
+        $projet ->nom = $request->nom;
+        $projet ->image = $request->image;
+        $projet ->objectif = $request->objectif;
+        $projet ->description = $request->description;
+        $projet->echeance = $request->echeance;
+        $projet->budget = $request->budget;
+        $projet->etat = $request->etat;
+        $projet->categorie_id = $request->categorie_id;
+        $projet->user_id = $request->user_id;
+        $projet->save();
+        return response()->json(['message' => 'Project create successfully', 'projet' => $projet], 201);
 
-        return response()->json($projet, 201);
     }
 
 
     /**
      * Display the specified resource.
      */
-    public function show(Projet $projet)
+    public function show(Projet $projet): \Illuminate\Http\JsonResponse
     {
         return response()->json($projet);
     }
@@ -61,30 +54,23 @@ class ProjetController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Projet $projet)
+    public function update(Request $request, Projet $projet): \Illuminate\Http\JsonResponse
     {
-        $request->validate([
-            'nom' => 'required',
-            'objectif' => 'required',
-            'description' => 'required',
-            'echeance' => 'required',
-            'budget' => 'required|numeric',
-            'etat' => 'in:Disponible,Financé',
-            'categories_id' => 'required|exists:categories,id',
-        ]);
 
         //On met à jour  un  projet existant
-        $projet = update([
-            'nom' => $request->name,
-            'objectif' => $request->objectif,
-            'description' => $request->description,
-            'echeance' => $request->echeance,
-            'budget' => $request->budget,
-            'etat' => $request->etat,
-            'categorie_id',
-        ]);
+            $projet = new Projet();
+            $projet ->nom = $request->nom;
+            $projet ->image = $request->image;
+            $projet ->objectif = $request->objectif;
+            $projet ->description = $request->description;
+            $projet->echeance = $request->echeance;
+            $projet->budget = $request->budget;
+            $projet->etat = $request->etat;
+            $projet->categorie_id = $request->categorie_id;
+            $projet->user_id = $request->user_id;
+            $projet->update();
+            return response()->json(['message' => 'Project update  successfully', 'projet' => $projet], 201);
 
-        return response()->json();
     }
 
     /**
